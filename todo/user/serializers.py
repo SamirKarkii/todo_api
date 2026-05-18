eeef0eee0c816b5f640e066d5f16a32434ae3412
Samir Karki <samirkarki9861975252@gmail.com>
2026-05-18 12:44:50 +0545

    field validation and object validation basics

--- todo/user/serializers.py
+++ todo/user/serializers.py
@@ -2,7 +2,24 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
-    class Meta: 
+
+    class Meta:
         model = Task
         fields = "__all__"
+
+    def validate_title(self, value):
+
+        if len(value) < 5:
+
+            raise serializers.ValidationError(
+                "Title must be at least 5 characters"
+            )
+
+        return value
     
+    def validate(self, data):
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError(
+                "description and title cannot be same "
+            )
+        return data
