d0f8427f0ff8a574d29503a8b28bfcffc4ef6f8e
Samir Karki <samirkarki9861975252@gmail.com>
2026-05-18 16:43:02 +0545

    create() and update() method unpacking serializers.save()'s behind the scence magic

--- todo/user/serializers.py
+++ todo/user/serializers.py
@@ -23,3 +23,13 @@ class TaskSerializer(serializers.ModelSerializer):
                 "description and title cannot be same "
             )
         return data
+    
+    def create(self,validated_data): #validated_data = clean input 
+        return Task.objects.create(**validated_data) # **validated_data = unpack dictionary , Task.objects.create()- db insert 
+    
+    def update(self,instance,validated_data):
+        instance.title = validated_data.get("title", instance.titile)
+        instance.description = validated_data.get("description", instance.description)
+        instance.completed = validated_data.get("completed",instance.completed)
+
+    
