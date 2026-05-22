7538e8101232461eb34c8e0e0d2ecccfe6862740
Samir Karki <samirkarki9861975252@gmail.com>
2026-05-22 17:04:46 +0545

    ordering and drf built in filtering

--- todo/user/api_view.py
+++ todo/user/api_view.py
@@ -108,21 +108,30 @@
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.filters import SearchFilter
 
 class ReadCreateTask(ListCreateAPIView):
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ["title"]
 
     def get_queryset(self):
-        queryset = Task.objects.all()
+        queryset = super().get_queryset()
+        
         completed = self.request.GET.get("completed")
-        title = self.request.GET.get("title")
+     #    title = self.request.GET.get("title")
+        ordering = self.request.GET.get("ordering")
 
         if completed is not None: 
             is_completed = completed.lower() == "true"
             queryset = queryset.filter(completed = is_completed)
 
-        if title is not None:  
-            queryset = queryset.filter(title__icontains=title)
+     #    if title is not None:  
+     #        queryset = queryset.filter(title__icontains=title)
+       
+        if ordering is not None: 
+            queryset = queryset.order_by(ordering)
 
         return queryset
 
